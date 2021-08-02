8f4fb5637958757d54215e5b7983c6b4f20bd5f4
Maximiano S. Ramiro <maximusterceiro3@hotmail.com>
2021-08-02 16:43:10 -0300

    estrutura que define a linguagem do site

--- core/Language.php
+++ core/Language.php
@@ -2,14 +2,30 @@
 class Language {
 
     private $l;
+    private $ini;
 
     public function __construct() {
         global $config;
         $this->l = $config['default_lang'];
 
-        if(!empyt($_SESSION['lang']) && file_exists('lang/'.$_SESSION['lang'].'.ini')) {
+        if(!empty($_SESSION['lang']) && file_exists('lang/'.$_SESSION['lang'].'.ini')) {
             $this->l = $_SESSION['lang'];
         }
+
+        $this->ini = parse_ini_file('lang'.$this->l.'.ini');
            
+    }
+
+    public function get($word, $return = false) {
+        $text = $word;
+        if(isset($this->ini[$word])) {
+            $text = $this->ini[$word];
+        }
+
+        if($return) {
+            return $text;
+        } else {
+            echo $text;
+        }
     }
 }
